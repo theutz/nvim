e4248c0e7e214bba19fb3a639f644e29b0b976e2
Michael Utz <michael@theutz.com>
2023-09-28 23:46:02 +0300

    refactor(autocmds): refactor to lua

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,17 +2,26 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  pattern = {
+    vim.fn.expand("~") .. "/.tmux.conf",
+    vim.fn.expand("~") .. "/.config/tmux/tmux.conf",
+  },
+  desc = "source tmux config files after writing",
+  group = vim.api.nvim_create_augroup("tmuxsource", { clear = true }),
+  command = [[! tmux source-file "%"]],
+})
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  pattern = {
+    vim.fn.expand("~") .. "/.local/share/chezmoi/*",
+  },
+  desc = "apply chezmoi changes on save",
+  group = vim.api.nvim_create_augroup("chezmoi", { clear = true }),
+  command = [[! chezmoi apply --source-path "%"]],
+})
+
 vim.cmd([[
-augroup tmuxsource
-  au!
-  autocmd BufWritePost ~/.tmux.conf ! tmux source "%"
-augroup end
-
-augroup chezmoi
-  au!
-  autocmd BufWritePost ~/.local/share/chezmoi/* ! chezmoi apply --source-path "%"
-augroup end
-
 augroup zsh
   au!
   autocmd BufNewFile,BufRead *.zsh,.zshrc,.zshenv,.zprofile,.zsh_aliases,.zlogin,.zlogout,.zpreztorc,*/.zprezto/*/functions/* set filetype=zsh
