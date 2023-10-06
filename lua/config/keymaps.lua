aa5677845d5f0ed4835df9cc1eac6a3dfbe6069f
Michael Utz <michael@theutz.com>
2023-10-06 11:10:10 +0300

    refactor(toggleterm): use builtin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,7 +5,7 @@
 vim.keymap.set("i", "<C-j>", "<esc>o<esc>kO<esc>ji", { desc = "Make room" })
 
 -- ToggleTerm over native LazyVim float_term
-vim.keymap.del("n", "<leader>ft")
-vim.keymap.del("n", "<leader>fT")
-vim.keymap.del("n", "<C-_>")
-vim.keymap.set("n", "<C-_>", "<cmd>ToggleTerm<cr>", { desc = "ToggleTerm" })
+-- vim.keymap.del("n", "<leader>ft")
+-- vim.keymap.del("n", "<leader>fT")
+-- vim.keymap.del("n", "<C-_>")
+-- vim.keymap.set("n", "<C-_>", "<cmd>ToggleTerm<cr>", { desc = "ToggleTerm" })
