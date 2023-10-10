18d206676007740df3480cab903182cb27549792
Michael Utz <michael@theutz.com>
2023-10-10 08:46:26 -0700

    refactor(autocmds): pluck vim api funcs
    Signed-off-by: Michael Utz <michael@theutz.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,34 +2,38 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+local expand = vim.fn.expand
+
+autocmd({ "BufWritePost" }, {
   pattern = {
-    vim.fn.expand("~") .. "/.tmux.conf",
-    vim.fn.expand("~") .. "/.config/tmux/tmux.conf",
+    expand("~") .. "/.tmux.conf",
+    expand("~") .. "/.config/tmux/tmux.conf",
   },
   desc = "source tmux config files after writing",
-  group = vim.api.nvim_create_augroup("tmuxsource", { clear = true }),
+  group = augroup("tmuxsource", { clear = true }),
   command = [[! tmux source-file "%"]],
 })
 
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+autocmd({ "BufWritePost" }, {
   pattern = {
-    vim.fn.expand("~") .. "/.local/share/chezmoi/*",
+    expand("~") .. "/.local/share/chezmoi/*",
   },
   desc = "apply chezmoi changes on save",
-  group = vim.api.nvim_create_augroup("chezmoi", { clear = true }),
+  group = augroup("chezmoi", { clear = true }),
   command = [[! chezmoi apply --source-path "%"]],
 })
 
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+autocmd({ "BufNewFile", "BufRead" }, {
   pattern = {
     "*.zsh",
-    vim.fn.expand("~") .. "/.z{log{in,out},zsh{rc,env},pr{ofile,eztorc},aliases}",
-    vim.fn.expand("~") .. "/.zprezto/runcoms/*",
-    vim.fn.expand("~") .. "/.zprezto/*/functions/*",
+    expand("~") .. "/.z{log{in,out},zsh{rc,env},pr{ofile,eztorc},aliases}",
+    expand("~") .. "/.zprezto/runcoms/*",
+    expand("~") .. "/.zprezto/*/functions/*",
   },
   desc = "set filetype for zsh files",
-  group = vim.api.nvim_create_augroup("zshfiles", { clear = true }),
+  group = augroup("zshfiles", { clear = true }),
   callback = function()
     if vim.bo.filetype == "markdown" then
       return
