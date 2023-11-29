3c6b702a7bb046712606b94498d928619747cbd5
Mike Utz <mike.u@delegator.com>
2023-11-29 11:43:09 +0300

    feat(keymaps): Add keymap for searching current buffer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,6 +5,7 @@
 vim.keymap.set("i", "<C-j>", "<esc>o<esc>kO<esc>ji", { desc = "Make room" })
 vim.keymap.set("n", "<leader>wv", "<cmd>vsp<cr>", { desc = "Split vertical" })
 vim.keymap.set("n", "<leader>ws", "<cmd>sp<cr>", { desc = "Split horizontal" })
+vim.keymap.set("n", "<leader>.", "<cmd>Telescope current_buffer_fuzzy_find<cr>", { desc = "Search curent buffer" })
 
 -- ToggleTerm over native LazyVim float_term
 -- vim.keymap.del("n", "<leader>ft")
