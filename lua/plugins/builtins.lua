7d33d82d1da704a4a39465ff85be1649c7527c05
Mike Utz <mike.u@delegator.com>
2023-12-06 13:32:28 +0300

    feat: Add delete buffer mappings to telescope.nvim

--- lua/plugins/builtins.lua
+++ lua/plugins/builtins.lua
@@ -1,6 +1,18 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        mappings = {
+          n = {
+            d = require("telescope.actions").delete_buffer,
+          },
+          i = {
+            ["<m-d>"] = require("telescope.actions").delete_buffer,
+          },
+        },
+      },
+    },
     keys = {
       {
         "<leader>;",
