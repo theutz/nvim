3ccd21d3fe8d0b28078a98040c83e6a3bb6c3bff
Mike Utz <mike.u@delegator.com>
2023-12-28 13:21:39 +0300

    feat: close neotree after opening file
    Signed-off-by: Mike Utz <mike.u@delegator.com>

--- lua/plugins/builtins.lua
+++ lua/plugins/builtins.lua
@@ -49,6 +49,14 @@ return {
           hide_dotfiles = false,
         },
       },
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function(file_path)
+            require("neo-tree.command").execute({ action = "close" })
+          end,
+        },
+      },
     },
   },
   {
