cdbeaebccc1a24a4fe259793bd0a4262ae6736e3
Mike Utz <mike.u@delegator.com>
2024-02-08 12:29:51 +0300

    feat: add "<leader>fs" commands to save files
    Signed-off-by: Mike Utz <mike.u@delegator.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,11 +3,11 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("i", "<C-j>", "<esc>o<esc>kO<esc>ji", { desc = "Make room" })
-vim.keymap.set("n", "<leader>wv", "<cmd>vsp<cr>", { desc = "Split vertical" })
-vim.keymap.set("n", "<leader>ws", "<cmd>sp<cr>", { desc = "Split horizontal" })
 vim.keymap.set("n", "<leader>.", "<cmd>Telescope current_buffer_fuzzy_find<cr>", { desc = "Search curent buffer" })
-
 vim.keymap.set("n", "<leader>ck", "<Cmd>LspRestart<cr>", { desc = "Restart LSP" })
-
-vim.keymap.set("n", "<leader>wo", "<Cmd>wincmd o<cr>", { desc = "Close Other Windows" })
+vim.keymap.set("n", "<leader>fs", "<cmd>w<cr>", { desc = "Save file" })
+vim.keymap.set("n", "<leader>fS", "<cmd>wa<cr>", { desc = "Save all files" })
 vim.keymap.set("n", "<leader>w_", "<Cmd>wincmd _<cr>", { desc = "Max out the height" })
+vim.keymap.set("n", "<leader>wo", "<Cmd>wincmd o<cr>", { desc = "Close Other Windows" })
+vim.keymap.set("n", "<leader>ws", "<cmd>sp<cr>", { desc = "Split horizontal" })
+vim.keymap.set("n", "<leader>wv", "<cmd>vsp<cr>", { desc = "Split vertical" })
