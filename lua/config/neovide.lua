5832603e5cfc192ed9a9d4951f5c090630ee0d16
Mike Utz <mike.u@delegator.com>
2024-02-13 14:02:20 +0300

    feat: split neovide config
    Signed-off-by: Mike Utz <mike.u@delegator.com>

---
+++ lua/config/neovide.lua
@@ -0,0 +1,16 @@
+vim.o.guifont = "Recursive:h16"
+
+local padding = 20
+vim.g.neovide_padding_top = padding
+vim.g.neovide_padding_bottom = padding
+vim.g.neovide_padding_left = padding
+vim.g.neovide_padding_right = padding
+
+vim.g.neovide_transparency = 0.95
+
+vim.g.neovide_input_macos_alt_is_meta = true
+
+vim.g.neovide_cursor_animation_length = 0.03
+vim.g.neovide_cursor_trail_size = 0.85
+
+vim.g.neovide_cursor_vfx_mode = "railgun"
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,25 +9,5 @@ vim.o.timeoutlen = 1000
 vim.o.ttimeoutlen = 0
 
 if vim.g.neovide then
-  vim.o.guifont = "MonaspiceRn Nerd Font:h12"
-
-  local padding = 20
-  vim.g.neovide_padding_top = padding
-  vim.g.neovide_padding_bottom = padding
-  vim.g.neovide_padding_left = padding
-  vim.g.neovide_padding_right = padding
-
-  local alpha = function()
-    return string.format("%x", math.floor((255 * vim.g.transparency) or 0.8))
-  end
-  vim.g.neovide_transparency = 0.0
-  vim.g.transparency = 0.9
-  vim.g.neovide_background_color = "#0f1117" .. alpha()
-
-  vim.g.neovide_input_macos_alt_is_meta = true
-
-  vim.g.neovide_cursor_animation_length = 0.03
-  vim.g.neovide_cursor_trail_size = 0.85
-
-  vim.g.neovide_cursor_vfx_mode = "railgun"
+  require("config.neovide")
 end
