5fa3674d2412b172fffc8fdde6eed4619c2cd728
Mike Utz <mike.u@delegator.com>
2024-02-14 17:02:48 +0300

    feat: better bindings for model.nvim
    Signed-off-by: Mike Utz <mike.u@delegator.com>

--- lua/plugins/model.lua
+++ lua/plugins/model.lua
@@ -7,6 +7,7 @@ return {
     config = function()
       require("which-key").register({
         { ["<leader>a"] = { name = "ai" } },
+        { ["<leader>ac"] = { name = "chat" } },
       })
     end,
     event = "VeryLazy",
@@ -20,9 +21,12 @@ return {
     end,
     ft = "mchat",
     keys = {
-      { "<leader>ad", "<cmd>Mdelete<cr>", mode = "n" },
-      { "<leader>as", "<cmd>Mselect<cr>", mode = "n" },
-      { "<leader>a<space>", "<cmd>Mchat<cr>", mode = "n" },
+      { "<leader>ad", "<cmd>Mdelete<cr>", mode = "n", desc = "Delete" },
+      { "<leader>as", "<cmd>Mselect<cr>", mode = "n", desc = "Select" },
+      { "<leader>ac4", "<cmd>Mchat gpt4<cr>", mode = "n", desc = "Chat with GPT-4" },
+      { "<leader>ac3", "<cmd>Mchat openai<cr>", mode = "n", desc = "Chat with GPT-3" },
+      { "<leader>acp", "<cmd>Mchat palm<cr>", mode = "n", desc = "Chat with Palm" },
+      { "<leader>aa", "<cmd>Mchat<cr>", mode = "n", desc = "Ask" },
     },
   },
 }
