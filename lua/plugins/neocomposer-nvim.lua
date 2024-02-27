8058559f9426ceeac87b574b5a51f22125874d4c
Mike Utz <mike.u@delegator.com>
2024-02-27 11:39:52 +0300

    feat: add neocomposer to telescope/lualine
    Signed-off-by: Mike Utz <mike.u@delegator.com>

--- lua/plugins/neocomposer-nvim.lua
+++ lua/plugins/neocomposer-nvim.lua
@@ -1,7 +1,12 @@
 return {
   {
     "ecthelionvi/NeoComposer.nvim",
-    dependencies = { "kkharji/sqlite.lua" },
+    dependencies = { "kkharji/sqlite.lua", "nvim-telescope/telescope.nvim" },
     opts = {},
+    config = function(_, opts)
+      require("NeoComposer").setup(opts)
+      require("telescope").load_extension "macros"
+      require("NeoComposer.ui").status_recording()
+    end,
   },
 }
