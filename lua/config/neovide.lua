0a85e663c0fa402b1b310766b299d020de166cb8
Mike Utz <mike.u@delegator.com>
2024-03-09 01:23:12 +0300

    feat: different transparency depending on background
    Signed-off-by: Mike Utz <mike.u@delegator.com>

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,5 +1,4 @@
-vim.o.guifont = "Macon:h16"
--- vim.o.linespace = 11
+vim.o.linespace = 0
 
 local padding = 20
 vim.g.neovide_padding_top = padding
@@ -7,8 +6,13 @@ vim.g.neovide_padding_bottom = padding
 vim.g.neovide_padding_left = padding
 vim.g.neovide_padding_right = padding
 
-vim.g.neovide_transparency = 0.80
-vim.g.neovide_window_blurred = true
+if vim.o.background == "light" then
+  vim.g.neovide_transparency = 0.93
+  vim.g.neovide_window_blurred = false
+elseif vim.o.background == "dark" then
+  vim.g.neovide_transparency = 0.8
+  vim.g.neovide_window_blurred = true
+end
 
 vim.g.neovide_input_macos_alt_is_meta = true
 
