7ee4de27a3ad9af44448389045ca373863d993e1
Michael Utz <michael@theutz.com>
2024-08-17 09:25:40 +0300

    chore: clean up unused color schemes
    Signed-off-by: Michael Utz <michael@theutz.com>

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,175 +1,23 @@
 -- vim: fdm=indent fdl=1
 local colors = {
   {
-    "ribru17/bamboo.nvim",
-    enabled = false,
-    opts = {
-      style = "multiplex",
-      transparent = true,
-      dim_inactive = true,
-      lualine = {
-        transparent = true,
-      },
-    },
-  },
-  {
-    "RRethy/base16-nvim",
-    enabled = false,
-  },
-  {
-    "catppuccin/nvim",
-    enabled = false,
-    name = "catppuccin",
-    opts = {
-      background = {
-        light = "latte",
-        dark = "mocha",
-      },
-    },
-  },
-  {
-    "scottmckendry/cyberdream.nvim",
-    enabled = false,
-    opts = {
-      transparent = true,
-      italic_comments = true,
-      hide_fillchars = true,
-      borderless_telescope = true,
-    },
-  },
-  {
-    "sainnhe/everforest",
-    enabled = false,
-    config = function()
-      vim.g.everforest_background = "hard"
-      vim.g.everforest_transparent_background = 0 -- 0, 1, 2
-    end,
-  },
-  {
-    "rebelot/kanagawa.nvim",
-    enabled = false,
-    config = true,
-    opts = {
-      style = "moon",
-      transparent = true,
-      dimInactive = false,
-    },
-  },
-  {
-    "dasupradyumna/midnight.nvim",
-    enabled = false,
-  },
-  {
-    "miikanissi/modus-themes.nvim",
-    enabled = false,
-  },
-  {
-    "olimorris/onedarkpro.nvim",
-    enabled = false,
-    opts = {
-      options = {
-        cursorline = false,
-        transparency = true,
-        lualine_transparency = true,
-      },
-    },
-  },
-  {
-    "nyoom-engineering/oxocarbon.nvim",
-    enabled = false,
-  },
-  {
-    "sainnhe/sonokai",
-    enabled = false,
-    config = function()
-      vim.g.sonokai_style = "atlantis" -- default, shusia, andromeda, atlantis, maia, espresso
-      vim.g.sonokai_better_performance = 1
-    end,
-  },
-  {
-    "ray-x/starry.nvim",
-    enabled = false,
-    config = function(_, opts)
-      local s = require "starry"
-      s.setup(opts)
-      vim.g.starry_disable_background = true
-    end,
-  },
-  {
-    "projekt0n/github-nvim-theme",
-    enabled = false,
-  },
-  {
     "folke/tokyonight.nvim",
-    enabled = false,
-    opts = {
-      transparent = true,
-      style = "moon",
-      light_style = "night",
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
-  },
-  {
-    "EdenEast/nightfox.nvim",
-    enabled = false,
-    opts = {
-      options = {
-        transparent = true,
-      },
-    },
-  },
-  {
-    "tiagovla/tokyodark.nvim",
-    enabled = false,
-    opts = {
-      transparent_background = true,
-    },
-  },
-  {
-    url = "https://codeberg.org/jthvai/lavender.nvim",
-    enabled = false,
-    branch = "stable", -- versioned tags + docs updates from main
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    "rose-pine/neovim",
-    enabled = false,
-    name = "rose-pine",
-    opts = {
-      dim_inactive_windows = true,
-      variant = "auto",
-      -- variant = "dawn",
-      -- dark_variant = "main",
-      styles = {
-        transparency = false,
-      },
-    },
-  },
-  {
-    "SethBarberee/challenger-deep.nvim",
-    enabled = false,
-    lazy = false,
-    priority = 1000,
-    name = "challenger-deep",
-  },
-  {
-    "0xstepit/flow.nvim",
     enabled = true,
     lazy = false,
     priority = 1000,
     opts = {
-      transparent = true,
-      fluo_color = "pink", -- pink, yellow, orange, green
-      mode = "bright", -- normal, bright, desaturate, or dark
+      transparent = false,
+      style = "moon",
+      light_style = "night",
+      styles = {
+        sidebars = "dark",
+        floats = "dark",
+      },
     },
   },
 }
 
-local colorscheme = "flow"
+local colorscheme = "tokyonight"
 
 local plugins = {
   {
