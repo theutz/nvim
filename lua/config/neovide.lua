43bfd4d5627253d72a9076b8288d0a9b4bd9373b
Michael Utz <michael@theutz.com>
2024-08-21 13:29:54 +0300

    chore: change neovide transparency

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,12 +1,13 @@
-vim.o.linespace = 20
-vim.o.guifont = "BlexMono Nerd Font:h16"
+vim.o.linespace = 0
+vim.o.guifont = "Lilex Nerd Font:h14"
 
 local padding = 20
 vim.g.neovide_padding_top = padding
 vim.g.neovide_padding_bottom = padding
 vim.g.neovide_padding_left = padding
 vim.g.neovide_padding_right = padding
-vim.g.neovide_transparency = 0.75
+
+vim.g.neovide_transparency = 0.90
 vim.g.neovide_window_blurred = true
 vim.g.neovide_input_macos_option_key_is_meta = "both"
 vim.g.neovide_cursor_animation_length = 0.03
